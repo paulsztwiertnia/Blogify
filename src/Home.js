0c3f1740421a86cd869483b9f82f4f5bc189ff98
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2024-03-12 19:25:48 -0400

    initial state

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to the Blog!</h2>
+            <p>This is a simple blog application built with React.</p>
+        </div>
+    );
+}
+
+export default Home;
